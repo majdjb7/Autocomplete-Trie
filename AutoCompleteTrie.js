5af77cdc3b4cdf93bc6d463195e8b84eddf971a2
majdjb7 <majdjd171@gmail.com>
2021-12-19 20:49:12 +0200

    add word and find word complete

--- AutoCompleteTrie.js
+++ AutoCompleteTrie.js
@@ -1,49 +1,68 @@
-const { boolean } = require("webidl-conversions");
-
 class AutoCompleteTrie {
     constructor(value) {
         this.value = value;
-        this.children ={};
+        this.children = {};
         this.endOfWord = false;
     }
 
-    putFlag(lastLetter) {
-        if(this.children[lastLetter]) {
-            this.children[lastLetter].endOfWord = true
-            console.log(this.children[lastLetter])
+    addLetterToTree(root, word) {
+        if(word.length == 1 && root.children[word]) {
+            root.children[word].endOfWord == true
+        }
+        if(!root.children[word[0]]) {
+            root.children[word[0]] = new AutoCompleteTrie(word[0])
+            if(word.length === 1) {
+                root.children[word[0]].endOfWord = true
+            }
+        }
+        else {
 
         }
+        if (word.length > 1) {
+            this.addLetterToTree(root.children[word[0]], word.slice(1));
+          }
     }
 
     addWord(word) {
-        for (let i=0; i<word.length; i++){
-            if(!this.children[word[i]] && i==0) {
-                this.children[word[i]] = new AutoCompleteTrie(word[i])
+        this.addLetterToTree(this, word)
+    }
 
-                this.children[word[i]].addWord(word.substring(1))
-            }
-            else if(this.children[word[i]]) {
-                // this.checkIfInChildren()
-                this.children[word[i]] = new AutoCompleteTrie(word[i])
-                this.children[word[i]].addWord(word.substring(1))
-            }
-            if(i == word.length-1) {
-                this.endOfWord = true
-            }
-            else{
-                this.endOfWord = false
-            }
+    findWordHelper(root, word) {
+        let result
+
+        if(!word || !root.children[word[0]]) {
+            return false
         }
-        this.putFlag(word[word.length-1])
-
+        if(root.children[word[0]].endOfWord == true && word.length == 1) {
+            return true
+        }
+        if(root.children[word[0]].value == word[0]) {
+            result = this.findWordHelper(root.children[word[0]], word.slice(1))
+            return result
+        }
+        else{
+            return false
+        }
+  
     }
 
     findWord(word) {
-
+        if(this.children[word[0]]) {
+            return this.findWordHelper(this.children[word[0]], word.slice(1))
+        }
+        else{
+            return false
+        }
     }
 
-    predictWord(word) {
+    
 
+    predictWord(word) {
+        let predictions = []
+        if(this.children[word[0]]) {
+            console.log(this.children[word[0]])
+        }
+        return predictions
     }
 }
 
--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const AutoCompleteTrie = require('./AutoCompleteTrie')
 
 let tree = new AutoCompleteTrie()
 tree.addWord("word")
+tree.addWord("wording")
+tree.addWord("woman")
 tree.addWord("adam")
 tree.addWord("words")
-// console.log(tree.children['w'].children['o'].children['r'].children['d'])
-console.log(tree.children['a'].children['d'].children['a'])
+tree.addWord("w")
 
-// console.log(tree.children['w'])
+// console.log(tree.findWord("word"))
+
+console.log(tree.children["w"].children["o"]);
+
+// console.log(tree.predictWord("wo"))
